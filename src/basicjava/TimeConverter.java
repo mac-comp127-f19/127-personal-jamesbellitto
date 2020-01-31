7f24ce2b0e3c70141be129d67f80c31237c94c9a
jbellitt <james.m.bellitto@gmail.com>
2020-01-31 10:41:36 -0600

    refactored expressions in expressionRefactoring

--- src/basicjava/TimeConverter.java
+++ src/basicjava/TimeConverter.java
@@ -1,22 +1,21 @@
 package basicjava;
 
 import java.util.Scanner;
-
-public class TimeConverter {
-    public static void main (String[]args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many hours?");
-        int hours = scanner.nextInt();
-        System.out.println("How many minutes?");
-        int minutes = scanner.nextInt();
-        System.out.print("How many seconds?");
-        int seconds = scanner.nextInt();
-
-        public static int TimeConverter(hours, minutes, seconds){
-            return (hours*360) + (minutes*60) + (seconds);
-
-        }
-    }
-
-
-}
+//
+//public class TimeConverter {
+//    public static void main (String[]args){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("How many hours?");
+//        int hours = scanner.nextInt();
+//        System.out.println("How many minutes?");
+//        int minutes = scanner.nextInt();
+//        System.out.print("How many seconds?");
+//        int seconds = scanner.nextInt();
+//
+//        return (hours*360) + (minutes*60) + (seconds);
+//
+//        }
+//    }
+//
+//
+//}
